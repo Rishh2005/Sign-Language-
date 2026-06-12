ef7b40921aa4e76866af458452d9ac3a1b3cdddc
Rishabh jain <rishabhjain11072005@gmail.com>
2026-06-13 00:03:08 +0530

    Update sign_language_training.py

--- sign_language_training.py
+++ sign_language_training.py
@@ -15,7 +15,7 @@ class SignLanguageDataCollector:
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(
             static_image_mode=False,
-            max_num_hands=1,
+            max_num_hands=1,  
             min_detection_confidence=0.7
         )
         self.mp_draw = mp.solutions.drawing_utils
